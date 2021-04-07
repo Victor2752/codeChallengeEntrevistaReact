65e2ff3add0c82042dbe2ffe0e720ee90e7a527d
Victor2752 <65194770+Victor2752@users.noreply.github.com>
2021-04-07 17:57:36 -0300

    Adição View 2
    criação do metodo para apresentar o View 2

---
+++ desafioentrevista/src/pages/FullPost/FullPost.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import axios from 'exios';
+
+class FullPost extends Component {
+    state={
+        loadedPost: noll
+    }
+
+    componentDidMount() {
+        if (this.props.match.params.id) { //verifica se o id não está nulo
+            if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
+                axios.get('/posts/' + this.props.match.params.id)
+                .then(response => {
+                    console.log(response.data, 'DARA');
+                    this.setState({ loadedPost: response.data})
+                })
+            }
+        }
+    }
+
+    render (){
+        let post = <p>Please select a Post!</p>;
+    
+        if(this.props.id) {
+            post = <p>Loading..!</p>;
+        }
+    
+        if(this.state.loadedPost){
+            post = (
+                <div>
+                    <h1>{this.state.loadedPost.title}</h1>
+                    <p>{this.state.loadedPost.body}</p>
+                </div>
+            )
+        }
+    }
+    
+}
+
